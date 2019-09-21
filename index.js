5732c95815e42dd486c2d035649150033f9cc753
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-09-21 23:14:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
- evenDefault()
+function preventRefreshOnSubmit(){
+    document.querySelector('form').addEventListener('submit', function(event){
+      event.preventDefault()
+    })
+}
+
+preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  return document.querySelector('input').value
+}
+
+function addNewElementAsLi() {
+  let n = retrieveEmployeeInformation();
+  return document.querySelector(".employee-list").append(n);
+}
+
+function addNewLiOnClick() {
+  document
+    .querySelector('input[type="submit"]')
+    .addEventListener('click', function(event) {
+      addNewElementAsLi();
+      resetInput();
+    });
+}
+
+function resetInput() {
+  document.querySelector("input").value = "";
+}
+
+function clearEmployeeListOnLinkClick() {
+  let link = document.querySelector('a');
+  let ul = document.querySelector('ul');
+
+  link.addEventListener('click', function(event) {
+    ul.innerHTML = ''
+  });
+}
